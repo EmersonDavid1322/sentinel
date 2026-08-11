aa5bdff743264367f0f77824fd4d52bc806cbf1e
EmersonDavid1322 <emer24820@gmail.com>
2026-08-10 19:52:39 -0600

    add: metodo striming añadido

--- src/rutas.cpp
+++ src/rutas.cpp
@@ -1,6 +1,11 @@
 #include <filesystem>
+#include "errores.h"
 namespace fs = std::filesystem;
 
 std::filesystem::path obtenerRutaBase(){
-    return  fs::canonical("/proc/self/exe").parent_path();
+    try {
+        return fs::canonical("/proc/self/exe").parent_path();
+    } catch (const std::filesystem::filesystem_error& e) {
+        throw ErrorConfig("No se pudo determinar la ruta base del ejecutable: " + std::string(e.what()));
+    }
 }
