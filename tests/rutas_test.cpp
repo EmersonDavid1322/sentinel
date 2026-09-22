3255e46fdf8e719433b8ce88d9ce95b9772c1b13
EmersonDavid1322 <emer24820@gmail.com>
2026-09-22 12:04:19 -0600

    fix: rutas

--- tests/rutas_test.cpp
+++ tests/rutas_test.cpp
@@ -1,3 +1,8 @@
 #include <gtest/gtest.h>
 #include "rutas.h"
 
+TEST(ValidarRutas, RutasSistema) {
+    EXPECT_EQ(obtenerRutaConfig(), "/etc/sentinel/sentinel.json");
+    EXPECT_EQ(obtenerRutaLogs(),"/var/log/sentinel");
+    EXPECT_EQ(obtenerRutaEstado(), "/var/lib/sentinel/");
+}
