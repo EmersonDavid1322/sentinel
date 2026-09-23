3616d6588f35d375cc1bf126871e783cd1bd050a
EmersonDavid1322 <emer24820@gmail.com>
2026-09-23 12:36:59 -0600

    prueba CLI test 4

--- tests/backup_nube_subida_test.cpp
+++ tests/backup_nube_subida_test.cpp
@@ -51,7 +51,7 @@ DatosRutas PrepararBackupNube(const std::filesystem::path& ruta_prueba) {
         carpeta_test_2 / "archivo_test_1.txt", carpeta_test_2 / "archivo_test_2.txt", carpeta_test_2 / "archivo_test_3.txt",
         subcarpeta_test / "archivo_test_1.txt", subcarpeta_test / "archivo_test_2.txt", subcarpeta_test / "archivo_test_3.txt",
         //archivos secundarios
-        subcarpeta_test / "archivo_log_test.log", carpeta_test_1 / "archivo_json_test.txt"
+        subcarpeta_test / "archivo_log_test.log"
     };
 
     for (const auto& archivo : archivos) {
